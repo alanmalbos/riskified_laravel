47275c948e9effa4e9359cfeb6143b1297f245df
Alan Malbos <alanmalbos@gmail.com>
2019-04-30 16:01:35 -0300

    first commit

---
+++ packages/Malbos/RiskifiedLaravel/src/RiskifiedLaravelServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+//Malbos\RiskifiedLaravel\src\RiskifiedLaravelServiceProvider.php
+
+namespace Malbos\RiskifiedLaravel;
+
+use Illuminate\Support\ServiceProvider;
+class RiskifiedLaravelServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+    }
+    public function register()
+    {
+    }
+}
